d074b231acd19251970b1f80dd21660702d7ccdd
mthierman <mthierman@gmail.com>
2024-07-31 13:38:09 -0600

    Fix user ctor params

--- src/plugin.cxx
+++ src/plugin.cxx
@@ -3,7 +3,7 @@
 namespace plugins::gain {
 struct Plugin final : public plugin::PluginHelper<Plugin, plugin::IgnoreNone> {
     explicit Plugin(const clap_host* host)
-        : plugin::PluginHelper<Plugin, plugin::IgnoreNone>(&plugin::descriptor, host) { }
+        : plugin::PluginHelper<Plugin, plugin::IgnoreNone>(host) { }
     ~Plugin() { }
 };
 } // namespace plugins::gain
