0c0c30556682aa4c2529585016def125a09c5fe0
mthierman <mthierman@gmail.com>
2024-07-31 14:36:34 -0600

    Remove gain namespace

--- src/plugin.cxx
+++ src/plugin.cxx
@@ -1,14 +1,12 @@
 #include <plugin/plugin.hxx>
 
-namespace gain {
 struct Plugin final : public plugin::PluginHelper<Plugin, plugin::IgnoreNone> {
     explicit Plugin(const clap_host* host)
         : plugin::PluginHelper<Plugin, plugin::IgnoreNone>(host) { }
     ~Plugin() { }
 };
-} // namespace gain
 
 auto plugin::create(const clap_host_t* host) -> const clap_plugin* {
-    auto plugin { new gain::Plugin(host) };
+    auto plugin { new Plugin(host) };
     return plugin->clapPlugin();
 }
