976b7fa195c8212bc89cfc01f5cfbd81c5090e87
mthierman <mthierman@gmail.com>
2024-08-01 00:22:15 -0600

    Fix entry

--- src/entry.cxx
+++ src/entry.cxx
@@ -1,4 +1,17 @@
 #include <plugin/entry.hxx>
+#include <plugin/factory.hxx>
+
+namespace plugin {
+namespace entry {
+    auto init(const char* /* plugin_path */) -> bool { return true; }
+
+    auto deinit(void) -> void { }
+
+    auto get_factory(const char* factory_id) -> const void* {
+        return (factory_id != CLAP_PLUGIN_FACTORY_ID) ? &plugin::clap_factory : nullptr;
+    }
+} // namespace entry
+} // namespace plugin
 
 extern "C" {
 const CLAP_EXPORT clap_plugin_entry clap_entry { .clap_version { CLAP_VERSION },
