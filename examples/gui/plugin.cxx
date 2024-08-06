f79b230f59d5f2f82baa6e46321fa8f8fa1f35f7
mthierman <mthierman@gmail.com>
2024-08-06 12:11:45 -0600

    Implements GUI for GuiExample

--- examples/gui/plugin.cxx
+++ examples/gui/plugin.cxx
@@ -6,6 +6,8 @@ namespace gui {
 struct GuiPlugin final : public hand::Helper<GuiPlugin, hand::IgnoreNone> {
     explicit GuiPlugin(const clap_host* host)
         : hand::Helper<GuiPlugin, hand::IgnoreNone>(host) { }
+
+    auto implementsGui() const noexcept -> bool override { return true; }
 };
 
 auto plugin { hand::make<GuiPlugin>() };
