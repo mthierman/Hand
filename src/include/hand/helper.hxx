7c8bf0ed203545f3afadef5a6fdac16379b67e14
mthierman <mthierman@gmail.com>
2024-08-07 09:27:34 -0600

    Move GUI to separate header

--- src/include/hand/helper.hxx
+++ src/include/hand/helper.hxx
@@ -8,54 +8,11 @@
 
 #include "descriptor.hxx"
 
-#if PLATFORM_WINDOWS
-#include <glow/filesystem.hxx>
-#include <glow/system.hxx>
-#include <glow/webview.hxx>
-#include <glow/window.hxx>
+#if defined(PLATFORM_WINDOWS)
+#include "gui_windows.hxx"
 #endif
 
 namespace hand {
-struct Window final : glow::window::Window {
-    Window() {
-        webViewEnvironment.m_userDataFolder
-            = glow::filesystem::known_folder(FOLDERID_LocalAppData, { "template-clap-plugin" });
-
-        message(WM_CREATE, [this](glow::messages::wm_create /* message */) {
-            glow::window::set_position(m_hwnd.get(), 0, 0, 640, 480);
-
-            webViewEnvironment.create([this]() {
-                webView.create(webViewEnvironment, m_hwnd.get(), [this]() {
-#if HOT_RELOAD
-                    webView.navigate(DEV_URL);
-#else
-                    webView.navigate("https://www.example.com/");
-#endif
-                    webView.put_bounds(m_hwnd.get());
-                });
-            });
-
-            return 0;
-        });
-
-        message(WM_WINDOWPOSCHANGED, [this](glow::messages::wm_windowposchanged /* message */) {
-            webView.put_bounds(m_hwnd.get());
-
-            return 0;
-        });
-
-        message(WM_DESTROY, [this](glow::messages::wm /* message */) {
-            webView.close();
-            webViewEnvironment.close();
-
-            return 0;
-        });
-    }
-
-    glow::webview::WebViewEnvironment webViewEnvironment;
-    glow::webview::WebView webView;
-};
-
 using TerminateMax = clap::helpers::Plugin<clap::helpers::MisbehaviourHandler::Terminate,
                                            clap::helpers::CheckingLevel::Maximal>;
 using TerminateMin = clap::helpers::Plugin<clap::helpers::MisbehaviourHandler::Terminate,
