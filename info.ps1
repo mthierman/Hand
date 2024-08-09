a625e601426ddaf9cbeef0a3c9a39a81e0807090
mthierman <mthierman@gmail.com>
2024-08-09 08:59:15 -0600

    Add clap-info script

---
+++ info.ps1
@@ -0,0 +1,9 @@
+Push-Location
+Set-Location build/examples/basic/Debug
+clap-info .\BasicPlugin.clap
+Pop-Location
+
+Push-Location
+Set-Location build/examples/gui/Debug
+clap-info .\GuiPlugin.clap
+Pop-Location
