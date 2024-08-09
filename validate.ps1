774d86a387c5c15aa7c9b5052e39c3dcbdd1da12
mthierman <mthierman@gmail.com>
2024-08-09 08:58:35 -0600

    Update validate script

--- validate.ps1
+++ validate.ps1
@@ -2,3 +2,8 @@ Push-Location
 Set-Location build/examples/basic/Debug
 clap-validator validate .\BasicPlugin.clap
 Pop-Location
+
+Push-Location
+Set-Location build/examples/gui/Debug
+clap-validator validate .\GuiPlugin.clap
+Pop-Location
