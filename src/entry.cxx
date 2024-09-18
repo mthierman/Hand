72a91c4c360a813fc03cb06467a52279daba2ab9
mthierman <mthierman@gmail.com>
2024-09-18 07:16:40 -0600

    Namespace entry together

--- src/entry.cxx
+++ src/entry.cxx
@@ -3,18 +3,15 @@
 
 #include <cstring>
 
-namespace hand {
-namespace entry {
-    auto init(const char* /* plugin_path */) -> bool { return true; }
+namespace hand::entry {
+auto init(const char* /* plugin_path */) -> bool { return true; }
 
-    auto deinit(void) -> void { }
+auto deinit(void) -> void { }
 
-    auto get_factory(const char* factory_id) -> const void* {
-        return (std::strcmp(factory_id, CLAP_PLUGIN_FACTORY_ID) == 0) ? &hand::clap_factory
-                                                                      : nullptr;
-    }
-} // namespace entry
-} // namespace hand
+auto get_factory(const char* factory_id) -> const void* {
+    return (std::strcmp(factory_id, CLAP_PLUGIN_FACTORY_ID) == 0) ? &hand::clap_factory : nullptr;
+}
+} // namespace hand::entry
 
 extern "C" {
 const CLAP_EXPORT clap_plugin_entry clap_entry { .clap_version { CLAP_VERSION },
--- src/include/hand/entry.hxx
+++ src/include/hand/entry.hxx
@@ -2,10 +2,8 @@
 
 #include <clap/entry.h>
 
-namespace hand {
-namespace entry {
-    auto init(const char* plugin_path) -> bool;
-    auto deinit(void) -> void;
-    auto get_factory(const char* factory_id) -> const void*;
-} // namespace entry
-} // namespace hand
+namespace hand::entry {
+auto init(const char* plugin_path) -> bool;
+auto deinit(void) -> void;
+auto get_factory(const char* factory_id) -> const void*;
+} // namespace hand::entry
