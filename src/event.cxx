fc6e6af58dc38e07c38519562d34644e3539d455
mthierman <mthierman@gmail.com>
2024-09-18 07:32:29 -0600

    Namespace events

--- src/event.cxx
+++ src/event.cxx
@@ -2,39 +2,37 @@
 
 #include <cassert>
 
-namespace hand {
-namespace event {
-    auto run_loop(const clap_process* process,
-                  std::function<void(const clap_event_header* event)> eventHandler)
-        -> clap_process_status {
-        if (process->audio_outputs_count <= 0) {
-            return CLAP_PROCESS_SLEEP;
-        }
-
-        auto ev { process->in_events };
-        auto sz { ev->size(ev) };
-
-        const clap_event_header* nextEvent { nullptr };
-        clap_id nextEventIndex { 0 };
-        if (sz != 0) {
-            nextEvent = ev->get(ev, nextEventIndex);
-        }
-
-        for (uint32_t i { 0 }; i < process->frames_count; ++i) {
-            while (nextEvent && nextEvent->time == i) {
-                eventHandler(nextEvent);
-                nextEventIndex++;
-                if (nextEventIndex >= sz) {
-                    nextEvent = nullptr;
-                } else {
-                    nextEvent = ev->get(ev, nextEventIndex);
-                }
-            }
-        }
-
-        assert(!nextEvent);
-
+namespace hand::event {
+auto run_loop(const clap_process* process,
+              std::function<void(const clap_event_header* event)> eventHandler)
+    -> clap_process_status {
+    if (process->audio_outputs_count <= 0) {
         return CLAP_PROCESS_SLEEP;
     }
-} // namespace event
-} // namespace hand
+
+    auto ev { process->in_events };
+    auto sz { ev->size(ev) };
+
+    const clap_event_header* nextEvent { nullptr };
+    clap_id nextEventIndex { 0 };
+    if (sz != 0) {
+        nextEvent = ev->get(ev, nextEventIndex);
+    }
+
+    for (uint32_t i { 0 }; i < process->frames_count; ++i) {
+        while (nextEvent && nextEvent->time == i) {
+            eventHandler(nextEvent);
+            nextEventIndex++;
+            if (nextEventIndex >= sz) {
+                nextEvent = nullptr;
+            } else {
+                nextEvent = ev->get(ev, nextEventIndex);
+            }
+        }
+    }
+
+    assert(!nextEvent);
+
+    return CLAP_PROCESS_SLEEP;
+}
+} // namespace hand::event
--- src/include/hand/event.hxx
+++ src/include/hand/event.hxx
@@ -5,10 +5,8 @@
 
 #include <functional>
 
-namespace hand {
-namespace event {
-    auto run_loop(const clap_process* process,
-                  std::function<void(const clap_event_header* event)> eventHandler)
-        -> clap_process_status;
-} // namespace event
-} // namespace hand
+namespace hand::event {
+auto run_loop(const clap_process* process,
+              std::function<void(const clap_event_header* event)> eventHandler)
+    -> clap_process_status;
+} // namespace hand::event
